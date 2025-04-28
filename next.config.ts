a18a50a6e73d469305f6f1117148be63903310d4
jschmidaguerre <juanseaguerre@gmail.com>
2025-04-28 20:05:07 -0300

    Configuración para deploy a GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from 'next-intl/plugin'; // asegurate que sea .js si usás MJS
  
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
-const nextConfig: import('next').NextConfig = {};
+const nextConfig = {
+  output: 'export' as const,
+  images: {
+    unoptimized: true
+  },
+  basePath: '/OCR-Page',
+  assetPrefix: '/OCR-Page'
+};
  
 export default withNextIntl(nextConfig);
